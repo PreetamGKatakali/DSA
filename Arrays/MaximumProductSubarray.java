c149050032413a4d5e24738fc8912071aed2be8e
PreetamGKatakali <preetam19cs051@gmail.com>
2023-12-06 19:54:14 +0530

    added

---
+++ Arrays/MaximumProductSubarray.java
@@ -0,0 +1,20 @@
+public class MaximumProductSubarray {
+    static int maxProductSubarray(int[] arr,int n){
+        int result=arr[0];
+        for(int i=0;i<n;i++){
+            int mul=arr[i];
+            for(int j=i+1;j<n;j++){
+                result=Math.max(result, mul);
+                mul*=arr[j];
+            }
+            result=Math.max(result, mul);
+
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] arr={6, -3, -10, 0, 2};
+        int res=maxProductSubarray(arr,arr.length);
+        System.out.println(res);
+    }
+}
